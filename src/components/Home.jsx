594495d1f9075fa59ef6efb4153eafbd18ae5fdf
Virginia Barbaro <virginiabarbaro@icloud.com>
2023-05-24 10:37:24 -0300

    responsive mobile reapir

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,63 +33,67 @@ function Home(props) {
 
   return (
     <>
-      <div className="d-flex">
-        <div className="row hero-image col-6"></div>
-        <h1 className="hero-title">Welcome to hacklist</h1>
-        <div className="col-6">
-          <div className="d-flex justify-content-center mt-4">
-            <Button
-              className="btn-add-list fs-4"
-              variant="ghost"
-              onClick={() => {
-                handleShow();
-              }}
-            >
-              <i className="bi bi-file-earmark-plus me-2"></i> Add new List
-            </Button>
+      <div>
+        <div className="row row-flex-general">
+          <div className="col-6 col-left-img">
+            <img className="hero-image"></img>
           </div>
-          <ListCard />
-          <Modal
-            show={show}
-            onHide={handleClose}
-            className="modal-container"
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-          >
-            <Modal.Header closeButton>
-              <Modal.Title>Title list</Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="modal-body">
-              <div className="modal-form">
-                <form onSubmit={(e) => handleSaveList(e)}>
-                  <label htmlFor="title-list" id="title-list"></label>
-                  <Input
-                    variant="flushed"
-                    placeholder="Type your title list.."
-                    value={listName}
-                    onChange={(e) => setListName(e.target.value)}
-                    type="text"
-                    name="listName"
-                    className="add-list-input"
-                  />
-
-                  <div className="btn-modal">
-                    <Button
-                      className="btn-back-modal"
-                      variant="ghost"
-                      onClick={handleClose}
-                    >
-                      <i className="bi bi-arrow-left me-2"></i>
-                      Back home
-                    </Button>
-                  </div>
-                </form>
-              </div>
-            </Modal.Body>
-          </Modal>
+          <h1 className="hero-title">Welcome to hacklist</h1>
+          <div className="col-6 col-right">
+            <div className="mt-4 text-center">
+              <Button
+                className="btn-add-list fs-4"
+                variant="ghost"
+                onClick={() => {
+                  handleShow();
+                }}
+              >
+                <i className="bi bi-file-earmark-plus me-2"></i> Add new List
+              </Button>
+            </div>
+          </div>
         </div>
+        <ListCard />
+        <Modal
+          show={show}
+          onHide={handleClose}
+          className="modal-container"
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Title list</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="modal-body">
+            <div className="modal-form">
+              <form onSubmit={(e) => handleSaveList(e)}>
+                <label htmlFor="title-list" id="title-list"></label>
+                <Input
+                  variant="flushed"
+                  placeholder="Type your title list.."
+                  value={listName}
+                  onChange={(e) => setListName(e.target.value)}
+                  type="text"
+                  name="listName"
+                  className="add-list-input"
+                />
+
+                <div className="btn-modal">
+                  <Button
+                    className="btn-back-modal"
+                    variant="ghost"
+                    onClick={handleClose}
+                  >
+                    <i className="bi bi-arrow-left me-2"></i>
+                    Back home
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </Modal.Body>
+        </Modal>
       </div>
     </>
   );
